189021f2003b22cb9ebbd095f1e243e2c7b484ba
victoria.visin <vvisintin18@gmail.com>
2023-02-12 12:08:24 -0300

    Modifico ItemsListDetail

--- src/componentes/ItemListDetail/ItemListDetail.jsx
+++ src/componentes/ItemListDetail/ItemListDetail.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ItemListDetail = () => {
+import { products } from "../../ProductsMock";
+
+import { useParams } from 'react-router-dom'
+
+import "./ItemListDetail.css"
+
+
+
+const ItemDetailContainer = () => {
+
+    const { id } = useParams()
+
+    const [product, setProduct] = useState({})
+
+
+    useEffect( ()=>{
+
+       let productSelected = products.find( prod => prod.id === Number(id) )
+
+        setProduct( productSelected )
+
+    },[])
+
+
   return (
-    <div>ItemListDetail</div>
+    <div className='cartDetail'>
+     <img src={product.img} alt="" height={300} />
+     <h1>{product.title}</h1>
+     <h3>{product.description}.</h3>
+     <h4>${product.price}</h4>
+     <button>Agregar al Carrito</button>
+
+
+    </div>
   )
 }
 
-export default ItemListDetail
+export default ItemDetailContainer
 
